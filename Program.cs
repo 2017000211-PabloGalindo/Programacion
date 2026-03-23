4ebef2ed021ace19051c5ea2e6553e5455042604
2017000211-PabloGalindo <p.galindo.5bdb@gmail.com>
2026-03-23 11:38:44 -0600

    Add files via upload
    TAREA

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("===MENSAJE DE VOZ===");
+        //Variable
+        int opc;
+        //Crear el menu
+        Console.WriteLine("MENU PRINCIPAL\n");
+        Console.WriteLine("1- Recepcio\n");
+        Console.WriteLine("2- Vigilancia\n");
+        Console.WriteLine("3- Mantenimiento\n");
+        Console.WriteLine("4- Salir\n");
+        Console.Write("Digite una Opcion []: ");
+        opc = Convert.ToInt32(Console.ReadLine());
+
+        //Condiciones
+        if (opc == 1)
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenido al Departamento Recepcion");
+        }
+        if (opc == 2)
+            Console.WriteLine("Bienvenido al Departamento Vigilancia");
+        if (opc == 3)
+            Console.WriteLine("Bienvenido al Departamento Mantenimiento");
+        if (opc != 1 && opc != 2 && opc != 3)
+            Console.WriteLine("Saliendo del Sistema");
+
+        //Limpiar pantalla
+        Console.Clear();
+    }
+}
