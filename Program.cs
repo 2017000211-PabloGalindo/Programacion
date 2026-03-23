89fea611a60e328b6a9cd9bf28496a28777ca415
2017000211-PabloGalindo <p.galindo.5bdb@gmail.com>
2026-03-23 11:53:13 -0600

    Add files via upload
    Act1Lec1Inc1 VISUAL STUDIO

--- Program.cs
+++ Program.cs
@@ -2,32 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("===MENSAJE DE VOZ===");
-        //Variable
-        int opc;
-        //Crear el menu
-        Console.WriteLine("MENU PRINCIPAL\n");
-        Console.WriteLine("1- Recepcio\n");
-        Console.WriteLine("2- Vigilancia\n");
-        Console.WriteLine("3- Mantenimiento\n");
-        Console.WriteLine("4- Salir\n");
-        Console.Write("Digite una Opcion []: ");
-        opc = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("===PROMEDIO DE NOTAS===");
+        int n1, n2, n3, n4, n5;
+        string materia;
+        Console.WriteLine("Digite la Materia: ");
+        materia =Console.ReadLine();
+        Console.WriteLine("Digite 5 notas: ");
+        n1=Convert.ToInt32(Console.ReadLine());
+        n2=Convert.ToInt32(Console.ReadLine());
+        n3=Convert.ToInt32(Console.ReadLine());
+        n4=Convert.ToInt32(Console.ReadLine());
+        n5=Convert.ToInt32(Console.ReadLine());
 
-        //Condiciones
-        if (opc == 1)
-        {
-            Console.Clear();
-            Console.WriteLine("Bienvenido al Departamento Recepcion");
-        }
-        if (opc == 2)
-            Console.WriteLine("Bienvenido al Departamento Vigilancia");
-        if (opc == 3)
-            Console.WriteLine("Bienvenido al Departamento Mantenimiento");
-        if (opc != 1 && opc != 2 && opc != 3)
-            Console.WriteLine("Saliendo del Sistema");
-
-        //Limpiar pantalla
-        Console.Clear();
+        double promedio = (n1+n2+n3+n4+n5)/5;
+        Console.WriteLine("La Materia:" + materia);
+        Console.WriteLine("El promedio:" + promedio);
     }
 }
